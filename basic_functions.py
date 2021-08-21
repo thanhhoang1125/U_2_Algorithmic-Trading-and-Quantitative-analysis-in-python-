3643c69718bc4574f25dfa65c535044066dbe28f
vavilapallivenkatsai <vavilapallivenkatsai@gmail.com>
2021-08-21 22:58:25 +0530

    Update basic_functions.py

--- basic_functions.py
+++ basic_functions.py
@@ -1,8 +1,6 @@
 # =============================================================================
 # Import OHLCV data and perform basic data operations
-# Author : Mayank Rasu (http://rasuquant.com/wp/)
 
-# Please report bug/issues in the Q&A section
 # =============================================================================
 
 import datetime as dt
